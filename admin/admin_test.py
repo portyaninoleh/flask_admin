41441cbaf61ee9533544cd129ee9ea95e467b4e6
oleh <ollieportnin@gmail.com>
2014-06-03 22:38:11 +0300

    Working on the flask-admin

--- admin/admin_test.py
+++ admin/admin_test.py
@@ -5,12 +5,16 @@ app = Flask(__name__)
 
 
 class AdminTestView(BaseView):
+    def is_accessible(self):
+        return login.current_user.
     @expose('/')
     def index(self):
         return self.render('index.html')
 
 
 admin = Admin(app)
-admin.add_view(AdminTestView(name='AdminTest'))
+admin.add_view(AdminTestView(name='Item1', endpoint='item1', category='DropDown'))
+admin.add_view(AdminTestView(name='Item2', endpoint='item2', category='DropDown'))
+admin.add_view(AdminTestView(name='Item3', endpoint='item3', category='DropDown'))
 
 app.run(debug=True)
